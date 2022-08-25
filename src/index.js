1bc6a9434acb45fa18ef7ed04eeb2bb8aa2667c9
DmytroVoitovych <begunec23@gmail.com>
2022-08-25 03:05:02 +0300

    ready to work

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// const axios = require('axios').default;
+
+
+
+// const genresF = (g) => console.log(g);
+
+// const getPost = async ()=> {
+//             try {
+//                 const response = await axios.get(`https://api.themoviedb.org/3/movie/${361743}?api_key=5b6b31e1fe12fe8c46151504ff095a5b`);
+//                   console.log(response);
+//               let  genres = response.data.genres.map((g, index, arr) => g.name + ', ').join('').slice(0, -2);
+//                genresF(genres);
+                
+//             } catch (error) {
+                 
+                          
+//             }
+              
+//         }
+
+//         getPost(); 
