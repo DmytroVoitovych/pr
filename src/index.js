0ca3efca05943a27a78f7a787bafde90be2f60bb
Morov78 <98202937+Morov78@users.noreply.github.com>
2022-08-26 17:07:35 +0300

    gallery+pagination

--- src/index.js
+++ src/index.js
@@ -1,18 +1,68 @@
+import { throttle } from 'lodash';
+import MovieAPiServer from './RequestApi/requestAPI';
+import { markupList } from './js/markupList';
+import { markupPaginationList } from './js/markupPaginationList';
 
-import MovieAPiServer from "./RequestApi/requestAPI";
+const movieAPiServer = new MovieAPiServer();
+//рефи
+const refs = {
+  galleryList: document.querySelector('.gallery-js'),
+  buttonPageTop: document.querySelector('.page-up'),
+  pagginationList: document.querySelector('.pagination-js'),
+};
+//прослуховувачі
+// window.addEventListener('scroll', throttle(onScroll, 500));
+// refs.buttonPageTop.addEventListener('click', onClickButtonPageTop);
+refs.pagginationList.addEventListener('click', onClickPagginationList);
 
+fetchData();
+
+function fetchData() {
+  movieAPiServer.fetchTopMovies().then(data => {
+    // console.log('page=', apiService.page, '  maxPages=', apiService.maxPages);
+    refs.galleryList.innerHTML = '';
+    refs.pagginationList.innerHTML = '';
+    renderMoviesList(data);
+  });
+}
+
+async function renderMoviesList(data) {
+  const genresList = await movieAPiServer.getGenresList();
+  // console.log(data, genresList);
+  refs.galleryList.insertAdjacentHTML(
+    'beforeend',
+    markupList(data, genresList)
+  );
+  refs.pagginationList.insertAdjacentHTML(
+    'beforeend',
+    markupPaginationList(movieAPiServer.page, movieAPiServer.maxPages)
+  );
+}
+//подія на кнопці пагінації
+function onClickPagginationList(event) {
+  const currentPage = event.target.dataset.page;
+  if (!currentPage) {
+    return;
+  }
+  movieAPiServer.pageCounter = Number(currentPage);
+
+  fetchData();
+  scrollToTop();
+}
+// перехід на верх сторінки
+function scrollToTop() {
+  window.scroll({
+    top: 0,
+    // behavior: 'smooth',
+  });
+}
 // Пример работы с запросами:
 
-const movieAPiServer = new MovieAPiServer;
-const fun = (e) => {
-    //  movieAPiServer.query = e.currentTarget.elements.... Значение с инпута
-    // movieAPiServer.query = "Spider";
-    // movieAPiServer.page = 1;
+const fun = e => {
+  //  movieAPiServer.query = e.currentTarget.elements.... Значение с инпута
+  // movieAPiServer.query = "Spider";
+  // movieAPiServer.page = 1;
 
-    movieAPiServer.fetchTopMovie().then(data => console.log(data.data))
-}
-fun();
-
-
-
-
+  movieAPiServer.fetchTopMovie().then(data => console.log(data.data));
+};
+// fun();
---
+++ src/js/markupPaginationList.js
@@ -0,0 +1,86 @@
+function markupPrevButton(page) {
+  //варіант коли кнопка є, але не клікабельна
+  return `<button type="button"
+  class="arrow pagination__button ${page === 1 ? 'no-active' : ''}"
+  data-page="${page - 1}"><</button>`;
+  //варіант коли стрілка зліва прибирається з рендеру
+  // return `${
+  //   page !== 1
+  //     ? `<button class="arrow pagination__button" type="button" data-page="${
+  //         page - 1
+  //       }"><</button>`
+  //     : ''
+  // }`;
+}
+
+function markupNextButton(page, maxPages) {
+  //варіант коли кнопка є, але не клікабельна
+  return `<button type="button"
+  class="arrow pagination__button ${page === maxPages ? 'no-active' : ''}"
+  data-page="${page + 1}">></button>`;
+  // варіант коли стрілка зправа прибирається з рендеру
+  // return `${
+  //   page === maxPages
+  //     ? ''
+  //     : `<button class="arrow pagination__button" type="button" data-page="${
+  //         page + 1
+  //       }">></button>`
+  // }`;
+}
+
+function arrayHelp(startIteration, finishIteration) {
+  const result = [];
+  for (let i = startIteration; i <= finishIteration; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+function markupButtons(page, maxPages) {
+  if (maxPages <= 9) {
+    return arrayHelp(1, maxPages);
+  }
+  if (page > maxPages - 2) {
+    return [1, '...', ...arrayHelp(maxPages - 4, maxPages)];
+  }
+  if (page >= maxPages - 4) {
+    return [
+      1,
+      '...',
+      ...arrayHelp(page - 2, page + 4).filter(item => item <= maxPages),
+    ];
+  }
+  if (page >= 6) {
+    return [1, '...', ...arrayHelp(page - 2, page + 2), '...', maxPages];
+  }
+  if (page < 6 && page >= 3) {
+    return [
+      ...arrayHelp(page - 4, page + 2).filter(item => item >= 1),
+      '...',
+      maxPages,
+    ];
+  }
+  if (page < 3) {
+    return [...arrayHelp(1, 5), '...', maxPages];
+  }
+}
+
+function markupPaginationList(page, maxPages) {
+  const markupButtonsList = markupButtons(page, maxPages)
+    .map(item => {
+      console.log(item);
+      return `<button type="button" class="pagination__button ${
+        page === item ? 'current' : ''
+      } ${
+        item === '...' ? 'no-active' : ''
+      }" data-page="${item}">${item}</button>`;
+    })
+    .join(' ');
+  return (
+    markupPrevButton(page) +
+    markupButtonsList +
+    markupNextButton(page, maxPages)
+  );
+}
+
+export { markupPaginationList };
