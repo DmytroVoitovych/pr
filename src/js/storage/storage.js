8846d653297f38703a2a21cba05926c0dc90598c
Nikita Zelenyak <Zelenyaknikita0927@gmail.com>
2022-08-31 22:21:09 +0800

    add Notiflix and prohibited add without auth

--- src/js/storage/storage.js
+++ src/js/storage/storage.js
@@ -1,10 +1,15 @@
 import { find } from "lodash";
+import { Notify,  } from "notiflix";
+
+
 
 export const watch = [].concat(JSON.parse(window.localStorage.getItem('watched')) ?? []); // база просмотреных
 export const queue = [].concat(JSON.parse(window.localStorage.getItem('queued')) ?? [] ) ; // база в очереди
 
 console.log(watch);
 console.log(queue);
+
+// console.log(auth);
 
 const funcUnic = (obj, id, list, ) => { // функция проверки обьекта на уникальность
   const checkId = obj => obj.id === id;
@@ -19,18 +24,27 @@ const funcUnic = (obj, id, list, ) => { // функция проверки об�
 };
 
 export const funcAddWatchBtn = (obj, btnW, btnQ, id) => { // функция добавления на страницу библиотекы данных
-        
+   
+
     btnW.addEventListener('click', () => {  // в список просмотреных
         
+ const auth = localStorage.getItem('auth');
+   
+        if (!auth) {
+     
+            return Notify.failure('Sorry, but you are not registered');
+  
+        }  
+
         if (!btnW.classList.contains('remove')) {
             btnW.classList.toggle('remove');    
             btnW.textContent = 'remove to Watched';
-            
+             Notify.success('Was added to watched');
         }
         else {
             btnW.classList.toggle('remove');
-           btnW.textContent = 'add to Watched';
-           
+            btnW.textContent = 'add to Watched';
+             Notify.success('Was deleted from watched');
         }
                 
         funcUnic(obj, id, watch, btnW);
@@ -40,20 +54,29 @@ export const funcAddWatchBtn = (obj, btnW, btnQ, id) => { // функция до
 
 
     btnQ.addEventListener('click', () => {  // в список очереди
-        
+         const auth = localStorage.getItem('auth');
+   
+        if (!auth) {
+     
+            return Notify.failure('Sorry, but you are not registered');
+  
+        }  
+
          if (!btnQ.classList.contains('removeQ')) {
             btnQ.classList.toggle('removeQ');    
             btnQ.textContent = 'remove to Queue';
             
+           Notify.success('Was added to queue');
         }
         else {
             btnQ.classList.toggle('removeQ');
            btnQ.textContent = 'add to Queue';
-           
+            Notify.success('Was deleted from queue');
         }
       
         funcUnic(obj, id, queue, btnQ);
         
-        return  window.localStorage.setItem(`queued`, JSON.stringify(queue));
+        window.localStorage.setItem(`queued`, JSON.stringify(queue));
+        
     });
 };
