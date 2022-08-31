6436f5f65450721d7268528143e54d071edf76f0
DmytroVoitovych <begunec23@gmail.com>
2022-08-31 16:14:31 +0300

    fix bugs btnHome

--- src/js/trackingFunFooter/trackingFunFooter.js
+++ src/js/trackingFunFooter/trackingFunFooter.js
@@ -4,10 +4,10 @@ import { refs } from "../refs"
 
 export const trackingFunFooter = () => {
     console.log(refs.galleryList.children.length);
-    if ( refs.galleryList.children.length === 0) {
+    if (refs.galleryList.children.length === 0) {
       
-        refs.galleryList.style.height =`32vh` ;
+        refs.galleryList.style.height = `32vh`;
     }
     
-       return
-}
+    return;
+};
